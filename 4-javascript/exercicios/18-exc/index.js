c2e4e6f13f49071a4a87a00a61a5e12ddf07ebac
devmathsena <dev.matheussena@gmail.com>
2025-03-26 22:40:20 -0300

    Update index.js

--- 4-javascript/exercicios/18-exc/index.js
+++ 4-javascript/exercicios/18-exc/index.js
@@ -5,6 +5,13 @@ const resultInput = document.getElementById('result')
 
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
+document.querySelectorAll('.charKey').forEach( function(charKeyBtn) {
+  charKeyBtn.addEventListener('click', function () {
+    const value = charKeyBtn.dataset.value
+    input.value += value
+  })
+})
+
 input.addEventListener('keydown', function (ev) {
   ev.preventDefault()
   if(allowedKeys.includes(ev.key)) {
@@ -14,4 +21,11 @@ input.addEventListener('keydown', function (ev) {
   if(ev.key === 'Backspace') {
     input.value = input.value.slice(0, -1)
   }
+  if(ev.key === 'Enter') {
+    caculate()
+  }
 })
+
+function caculate() {
+  console.log("Calculado")
+}
