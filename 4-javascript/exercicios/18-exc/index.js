40d0e92a7488e46c47884882d28286485ae8d64a
devmathsena <dev.matheussena@gmail.com>
2025-03-26 22:43:21 -0300

    Update index.js
    Criação de evento para clear button

--- 4-javascript/exercicios/18-exc/index.js
+++ 4-javascript/exercicios/18-exc/index.js
@@ -12,6 +12,11 @@ document.querySelectorAll('.charKey').forEach( function(charKeyBtn) {
   })
 })
 
+document.getElementById('clear').addEventListener('click', function () {
+  input.value = ''
+  input.focus()
+})
+
 input.addEventListener('keydown', function (ev) {
   ev.preventDefault()
   if(allowedKeys.includes(ev.key)) {
