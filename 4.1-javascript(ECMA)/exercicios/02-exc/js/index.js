3c7371f9bd109480e9dae348142f753038fd6cb4
devmathsena <dev.matheussena@gmail.com>
2025-04-27 09:15:49 -0300

    Update index.js

--- 4.1-javascript(ECMA)/exercicios/02-exc/js/index.js
+++ 4.1-javascript(ECMA)/exercicios/02-exc/js/index.js
@@ -1,35 +1,15 @@
 import caculate from "./calculate.js"
 import copyToClipBoard from "./copyToClipBoard.js"
 import themeSwitcher from "./themeSwitcher.js"
+import { handleButtonPress, handlerClearButton, handleTyping } from "./keyHandlers.js"
 
-const input = document.getElementById('input')
-const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
-
-document.querySelectorAll('.charKey').forEach( function(charKeyBtn) {
-  charKeyBtn.addEventListener('click', function () {
-    const value = charKeyBtn.dataset.value
-    input.value += value
-  })
+document.querySelectorAll('.charKey').forEach(function(charKeyBtn) {
+  charKeyBtn.addEventListener('click', handleButtonPress)
 })
 
-document.getElementById('clear').addEventListener('click', function () {
-  input.value = ''
-  input.focus()
-})
+document.getElementById('clear').addEventListener('click', handlerClearButton)
 
-input.addEventListener('keydown', function (ev) {
-  ev.preventDefault()
-  if(allowedKeys.includes(ev.key)) {
-    input.value += ev.key
-    return
-  }
-  if(ev.key === 'Backspace') {
-    input.value = input.value.slice(0, -1)
-  }
-  if(ev.key === 'Enter') {
-    caculate()
-  }
-})
+document.getElementById('input').addEventListener('keydown', handleTyping)
 
 document.getElementById('equal').addEventListener('click', caculate)
 
