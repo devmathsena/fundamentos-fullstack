cab53a158d621b0381126bef6b94c1bed78c74a9
devmathsena <dev.matheussena@gmail.com>
2025-04-28 22:01:16 -0300

    Update index.js

--- 4.1-javascript(ECMA)/exercicios/04-exc/index.js
+++ 4.1-javascript(ECMA)/exercicios/04-exc/index.js
@@ -1,1 +1,20 @@
 const dayjs = require('dayjs')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+dayjs.extend(customParseFormat)
+
+function nextBirthday(birthdate) {
+    const birthdateFormated = dayjs(birthdate, 'DD/MM/YYYY')
+    const day = birthdateFormated.get('date')
+    const month = birthdateFormated.get('month')
+    const nextYear = dayjs().add(1, 'year').get('year')
+    const currentDate = dayjs()
+
+    const nextBirthday = `${day}/${month+1}/${nextYear}`
+    const nextBirthdayzz = dayjs(nextBirthday, 'DD/M/YYYY')
+    const difference = currentDate.diff(nextBirthdayzz, 'day')
+    
+    console.log(`Sua próxima data de aniversário: ${nextBirthday}`);
+    console.log(`Falta ${difference} para seu aniversário.`);
+}
+
+nextBirthday("21/03/1998")
